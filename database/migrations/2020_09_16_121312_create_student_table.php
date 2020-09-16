21cf304bf7ee6fe6792e56cf64fbdc40a19ba823
kirti.sonawane <kirti.sonawane@unimoni.com>
2020-09-16 19:03:31 +0530

    Laravel 7 Crud

---
+++ database/migrations/2020_09_16_121312_create_student_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('roll_no');
+            $table->string('subject');
+            $table->string('address');            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+}
